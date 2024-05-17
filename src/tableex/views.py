bb7b8d77a7d0167f5ed3f85ba0f14cb6b648a3f6
quote-meta <nullpo.loveletter.vbs@gmail.com>
2024-05-18 00:13:53 +0900

    feat: table generator

--- src/tableex/views.py
+++ src/tableex/views.py
@@ -1,6 +1,16 @@
 from flask import render_template
 from tableex import app
 
+rows = 256
+columns = 16
+header = [f'header{column}' for column in range(columns)]
+body = [[f'data[{row},{column}]' for column in range(columns)] for row in range(rows)]
+
+table_data = {
+    'header': header,
+    'body': body
+}
+
 @app.route('/')
 def index():
-    return render_template('tableex/index.html')
+    return render_template('tableex/index.html', table_data=table_data)
